207b01362072e3b018239d675105d15522253919
GFraser101 <greg_fraser@dell.com>
2022-03-15 14:56:59 +1000

    Update awx-install.sh

--- awx-install.sh
+++ awx-install.sh
@@ -29,7 +29,7 @@ read -sp "Password:" DH_PASS
 echo -e "\n\nClean up from any previous script runs..." | tee -a awx-install.log
 
 # Remove previous log file
-NOW=$(date "DATE_FORMAT")
+NOW=$(date "$DATE_FORMAT")
 if sudo test -a "./awx-install.log"
 then
 	echo -e "\n\n$NOW - Remove previous install log..." | tee -a awx-install.log
@@ -43,7 +43,7 @@ then
 fi
 
 # Remove cached docker hub credentials
-NOW=$(date "DATE_FORMAT")
+NOW=$(date "$DATE_FORMAT")
 if sudo test -a "/root/.docker/config.json"
 then
 	echo -e "\n\n$NOW - Remove cached docker hub credentials..." | tee -a awx-install.log
@@ -57,7 +57,7 @@ then
 fi	
 
 # Remove AWX clone
-NOW=$(date "DATE_FORMAT")
+NOW=$(date "$DATE_FORMAT")
 if sudo test -a "./awx"
 then
 	echo -e "\n\n$NOW - Remove previous AWX clone..." | tee -a awx-install.log
@@ -75,7 +75,7 @@ fi
 echo -e "\n\nRunning installation tasks..." | tee -a awx-install.log
 
 # Linux Update
-NOW=$(date "DATE_FORMAT")
+NOW=$(date "$DATE_FORMAT")
 echo -e "\n\n$NOW - Linux update..." | tee -a awx-install.log
 
 if sudo apt-get update | tee -a awx-install.log > /dev/null; then
@@ -91,7 +91,7 @@ else
 fi
 
 # Install Docker
-NOW=$(date "DATE_FORMAT")
+NOW=$(date "$DATE_FORMAT")
 echo -e "\n\n$NOW - Install Docker..." | tee -a awx-install.log
 
 if sudo apt-get install docker docker.io -y | tee -a awx-install.log > /dev/null; then
@@ -107,7 +107,7 @@ else
 fi
 
 # Install Docker Compose
-NOW=$(date "DATE_FORMAT")
+NOW=$(date "$DATE_FORMAT")
 echo -e "\n\n$NOW - Install Docker compose..." | tee -a awx-install.log
 
 if sudo pip3 install docker-compose | tee -a awx-install.log > /dev/null; then
@@ -123,7 +123,7 @@ else
 fi
 
 # Set docker user access
-NOW=$(date "DATE_FORMAT")
+NOW=$(date "$DATE_FORMAT")
 echo -e "\n\n$NOW - Set Docker user access..." | tee -a awx-install.log
 
 if sudo usermod -aG docker $USER | tee -a awx-install.log > /dev/null; then
@@ -139,7 +139,7 @@ else
 fi
 
 # Git clone awx ansible install
-NOW=$(date "DATE_FORMAT")
+NOW=$(date "$DATE_FORMAT")
 echo -e "\n\n$NOW - Git clone awx ansible install..." | tee -a awx-install.log
 
 if sudo git clone --progress -b  17.0.1 https://github.com/ansible/awx.git &>> awx-install.log; then
@@ -155,7 +155,7 @@ else
 fi
 
 # Edit inventory file - set awx host_port=8080
-NOW=$(date "DATE_FORMAT")
+NOW=$(date "$DATE_FORMAT")
 echo -e "\n\n$NOW - Edit inventory file - set awx host_port=8080..." | tee -a awx-install.log
 
 if sudo sed -i -e 's/.*host_port=.*/host_port=8080/' ./awx/installer/inventory | tee -a awx-install.log > /dev/null; then
@@ -171,7 +171,7 @@ else
 fi
 
 # Edit inventory file - set awx admin_password
-NOW=$(date "DATE_FORMAT")
+NOW=$(date "$DATE_FORMAT")
 echo -e "\n\n$NOW - Edit inventory file - set awx admin_password..." | tee -a awx-install.log
 
 if sudo sed -i -e 's/.*admin_password=.*/admin_password=Password123!/' ./awx/installer/inventory | tee -a awx-install.log > /dev/null; then
@@ -187,7 +187,7 @@ else
 fi
 
 # Edit inventory file - set awx create_preload_data=false
-NOW=$(date "DATE_FORMAT")
+NOW=$(date "$DATE_FORMAT")
 echo -e "\n\n$NOW - Edit inventory file - set awx create_preload_data=false..." | tee -a awx-install.log
 
 if sudo sed -i -e 's/.*create_preload_data=.*/create_preload_data=false/' ./awx/installer/inventory | tee -a awx-install.log > /dev/null; then
@@ -203,7 +203,7 @@ else
 fi
 
 # Docker hub login
-NOW=$(date "DATE_FORMAT")
+NOW=$(date "$DATE_FORMAT")
 echo -e "\n\n$NOW - Docker hub login..." | tee -a awx-install.log
 
 if sudo docker login -u $DH_USER -p $DH_PASS &>> awx-install.log; then
@@ -219,7 +219,7 @@ else
 fi
 
 # Run awx install playbook
-NOW=$(date "DATE_FORMAT")
+NOW=$(date "$DATE_FORMAT")
 echo -e "\n\n$NOW - Run awx install playbook.  This may take some time (20mins)..." | tee -a awx-install.log
 
 if sudo ansible-playbook -i ./awx/installer/inventory ./awx/installer/install.yml | tee -a awx-install.log > /dev/null; then
@@ -235,7 +235,7 @@ else
 fi
 
 # Install  succeeded
-NOW=$(date "DATE_FORMAT")
+NOW=$(date "$DATE_FORMAT")
 echo -e "\n\n$NOW - AWX has installed successfully..." | tee -a awx-install.log
 
 echo -e "\n\nFrom the Launch Pad web browser connect to http://192.168.1.238:8080\n\n" | tee -a awx-install.log
